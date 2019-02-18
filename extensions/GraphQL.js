39bce00b1c6b88a29f7855c6032a0fcf2d8143f0
Grzegorz <stysiok@o2.pl>
2019-02-18 22:24:36 +0100

    CodeScanner styles and logic

---
+++ extensions/GraphQL.js
@@ -0,0 +1,18 @@
+export const GraphQLFetch = async(query) => {
+    try {
+        let response = await fetch('http://nextlevelbjjtest.azurewebsites.net/graphql/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                 query: query
+            })
+        });
+
+        return await response.json();
+
+    } catch (error) {
+        console.log(error);
+    }
+};
--- screens/CodeScanner.js
+++ screens/CodeScanner.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Alert, Linking, Dimensions, LayoutAnimation, Text, View, StatusBar, StyleSheet, TouchableOpacity } from 'react-native';
+import { Dimensions, LayoutAnimation, Text, View, StatusBar, StyleSheet } from 'react-native';
 import { BarCodeScanner, Permissions } from 'expo';
+import { GraphQLFetch } from '../extensions/GraphQL';
 
 export default class App extends Component {
   state = {
@@ -19,59 +20,50 @@ export default class App extends Component {
     });
   };
 
-  _handleBarCodeRead = result => {
-    console.log(this.state.lastScannedValue)
+  _handleBarCodeRead = async(result) => {
+    console.log(result);
     if (result.data !== this.state.lastScannedValue) {
       LayoutAnimation.spring();
       this.setState({ lastScannedValue: result.data });
     }
-    console.log(result);
+
+    var data = await GraphQLFetch(`{
+      getStudentByPassId(passCode:"NL096") {
+        firstName
+        lastName
+      }
+    }`);
+    console.log(data.data.getStudentByPassId.firstName);
   };
 
   render() {
     return (
       <View style={styles.container}>
-
         {this.state.hasCameraPermission === null
-          ? <Text>Requesting for camera permission</Text>
+          ? <Text>Potrzebujemy Twojego aparatu, żeby zeskanować Twój kod QR.</Text>
           : this.state.hasCameraPermission === false
               ? <Text style={{ color: '#fff' }}>
-                  Camera permission is not granted
+                  Nie dałeś udzieliłeś przyzwolenia, więc jak chcesz zczytać informację o karnecie?
                 </Text>
               : <BarCodeScanner
                   onBarCodeRead={this._handleBarCodeRead}
                   style={StyleSheet.absoluteFill}>
-                  <View style={styles.margins}></View>
+                  <View style={styles.margins}>
+                  <Text style={styles.topText}>Zeskanuj swój kod QR z karnetu</Text>
+                  </View>
                   <View style={styles.inside}>
                     <View style={styles.side}></View>
                     <View style={styles.highlight}></View>
                     <View style={styles.side}></View>
                   </View>
-                  <View style={styles.margins}></View>
+                  <View style={styles.margins}>
+                  <Text style={styles.bottomText} onPress={() => this.props.navigation.pop()}>Powrót</Text>
+                  </View>
                   </BarCodeScanner>}
-        <StatusBar hidden />
+        <StatusBar />
       </View>
     );
   }
-
-  _handlePressUrl = () => {
-    Alert.alert(
-      'Open this URL?',
-      this.state.lastScannedValue,
-      [
-        {
-          text: 'Yes',
-          onPress: () => Linking.openURL(this.state.lastScannedValue),
-        },
-        { text: 'No', onPress: () => {} },
-      ],
-      { cancellable: false }
-    );
-  };
-
-  _handlePressCancel = () => {
-    this.setState({ lastScannedValue: null });
-  };
 }
 
 const { width } = Dimensions.get('window');
@@ -85,16 +77,30 @@ const styles = StyleSheet.create({
   }, 
   margins:{
     flex: 3,    
-    backgroundColor: opacity
+    backgroundColor: opacity,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  topText: {
+    fontSize: width * 0.09,
+    textAlign: 'center',
+    width: '70%',
+    color: 'white',
   },
   inside: {
     flex: 4,
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between', 
   },
   side: {
     flex: 1, 
     backgroundColor: opacity
+  },
+  bottomText:{
+    fontSize: width * 0.07,
+    textAlign: 'center',
+    width: '70%',
+    color: 'white',
   },
   highlight: {
     flex: 8
